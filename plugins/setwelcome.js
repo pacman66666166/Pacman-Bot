b7e771a7667743434d703c043bd0445789a9c2e5
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 16:52:56 -0600

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     m.reply('Welcome se a cambiado a:\n@user (Mention)\n@subject (nombre del grupo )')
   } else throw 'Y el texto ?'
 }
-handler.help = ['setwelcome <teks>']
+handler.help = ['setwelcome ᴱˢᶜʳᶦᵇᵉ ˡᵃ ᴮᶦᵉⁿᵛᵉⁿᶦᵈᵃ']
 handler.tags = ['owner', 'group']
 
 handler.command = /^setwelcome$/i
