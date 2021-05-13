a2b0681fbfe225499dd3158ce53f6eb44e8226f7
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 16:27:08 -0600

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,10 +1,11 @@
 let handler = async m => m.reply(`
- *GIT DE ESTE BOT*
-https://github.com/Samu330/esubot
+*SIGUEMEEEEEE*
+
+https://www.instagram.com/susana_monterroza_/
 
 MY GIT:
 
-https://github.com/Samu330
+https://github.com/Susi666
 😴                                       
 `.trim()) 
 handler.help = ['git']
