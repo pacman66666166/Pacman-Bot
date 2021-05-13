ac61941c468a4af6fa2a692985d303fa189b2dba
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 17:27:10 -0600

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -16,7 +16,7 @@ let handler  = async (m, { conn, text }) => {
     })
   }
 }
-handler.help = ['ttp <teks>']
+handler.help = ['ttp ᴴᵃᶜᵉ ᵉˡ ˢᵗᶦᶜᵏᵉʳ ᵉⁿ ᵗᵉˣᵗᵒ ᵗᵃᵐᵇᶦᵉⁿ']
 handler.tags = ['sticker']
 handler.command = /^ttp$/i
 handler.owner = false
