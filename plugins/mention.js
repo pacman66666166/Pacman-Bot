36034fce0c84b22710856a309823d41a6a2f44dd
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 16:39:37 -0600

    Update mention.js

--- plugins/mention.js
+++ plugins/mention.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
     }
   })
 }
-handler.help = ['mention <teks>']
+handler.help = ['mention ᴱᵗᶦᑫᵘᵉᵗᵃ ᵃ ᵗᵒᵈᵒˢ']
 handler.tags = ['tools']
 
 handler.command = /^mentiin$/i
