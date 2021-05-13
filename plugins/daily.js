a2a47ba6b4fef7e055fa1f154e27cf777773257c
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 16:13:10 -0600

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
-  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
-    global.DATABASE._data.users[m.sender].exp += 500
-    m.reply('+500')
+  if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 8640000000) {
+    global.DATABASE._data.users[m.sender].exp += 90000000
+    m.reply('90000000')
     global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
   } else m.reply('Ya has reclamado hoy:)')
 }
-handler.help = ['daily', 'claim']
+handler.help = ['daily ᴿᵉᶜˡᵃᵐᵃ ˣᵖ ˢᵒˡᵒ ᵘⁿᵃ ᵛᵉᶻ ᵖᵒʳ ᵈᶦᵃ', 'claim ᴿᵉᶜˡᵃᵐᵃ ˣᵖ ˢᵒˡᵒ ᵘⁿᵃ ᵛᵉᶻ ᵖᵒʳ ᵈᶦᵃ']
 handler.tags = ['xp']
 handler.command = /^(daily|claim)$/i
 handler.owner = false
