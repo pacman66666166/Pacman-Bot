a7b773200de2e398a5bf9ae3ccece7090290fe59
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-05-13 20:35:04 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Susi\n❏ 𝐿𝑎 𝑓𝑒𝑙𝑖𝑐𝑖𝑑𝑎𝑑 𝑛𝑜 𝑒𝑠 𝑒𝑙 𝑜𝑏𝑗𝑒𝑡𝑖𝑣𝑜...\n❏ 𝐄𝐬 𝐞𝐥 𝐜𝐚𝐦𝐢𝐧𝐨\n⚠️Susi.li_⚠️ '
-global.author = '➬🌤️SusiBot🌤️\n↳  🔥+49 172 43333338🥀\n\n\n\n\n      🌺Susana_monterroza_🌺\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.packname = 'Susi\n❏ 𝐿𝑎 𝑓𝑒𝑙𝑖𝑐𝑖𝑑𝑎𝑑 𝑛𝑜 𝑒𝑠 𝑒𝑙 𝑜𝑏𝑗𝑒𝑡𝑖𝑣𝑜...\n❏ 𝐄𝐬 𝐞𝐥 𝐜𝐚𝐦𝐢𝐧𝐨\n⚠️苏珊娜⚠️ '
+global.author = '➬🌤️SusiBot🌤️\n↳  🔥+49 172 43333338🥀\n↳  🌺Susi.li_🌺'
 
 global.multiplier = 69 // The higher, The harder levelup
 
